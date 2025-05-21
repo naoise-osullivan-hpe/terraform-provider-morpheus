0457901e953e864a26319e6a584b69ee1ede59d6
Jason Quigley <138007593+jasonquigleyhpecom@users.noreply.github.com>
2025-05-21 13:47:20 +0100

    remove goto statements (#50)
    * remove goto statements
    
    * named function
    
    * Break out get functions
    
    * small refactor

--- internal/subproviders/morpheus/datasources/group/datasource.go
+++ internal/subproviders/morpheus/datasources/group/datasource.go
@@ -2,14 +2,15 @@ package group
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"net/http"
 
 	"github.com/HewlettPackard/hpe-morpheus-go-sdk/sdk"
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
-	"github.com/hashicorp/terraform-plugin-framework/types"
 
 	"github.com/HPE/terraform-provider-hpe/internal/subproviders/morpheus/configure"
+	"github.com/HPE/terraform-provider-hpe/internal/subproviders/morpheus/convert"
 	"github.com/HPE/terraform-provider-hpe/internal/subproviders/morpheus/datasources/group/consts"
 )
 
@@ -49,20 +50,60 @@ func (d *DataSource) Schema(
 	resp.Schema = GroupDataSourceSchema(ctx)
 }
 
-func strToType(s *string) types.String {
-	if s == nil {
-		return types.StringNull()
+func getGroupByID(
+	ctx context.Context,
+	id int64,
+	apiClient *sdk.APIClient,
+) (*sdk.ListGroups200ResponseAllOfGroupsInner, error) {
+	g, hresp, err := apiClient.GroupsAPI.GetGroups(ctx, id).Execute()
+	if err != nil || hresp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("GET failed for group %d", id)
 	}
 
-	return types.StringValue(*s)
+	group := g.GetGroup()
+
+	return &group, nil
 }
 
-func int64ToType(i *int64) types.Int64 {
-	if i == nil {
-		return types.Int64Null()
+func getGroupByName(
+	ctx context.Context,
+	name string,
+	apiClient *sdk.APIClient,
+) (*sdk.ListGroups200ResponseAllOfGroupsInner, error) {
+	gs, hresp, err := apiClient.GroupsAPI.ListGroups(ctx).Name(name).Execute()
+	if err != nil || hresp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("GET failed for group %s", name)
 	}
 
-	return types.Int64Value(*i)
+	var groups []sdk.ListGroups200ResponseAllOfGroupsInner
+
+	for _, g := range gs.Groups {
+		if g.GetName() == name {
+			groups = append(groups, g)
+		}
+	}
+
+	if len(groups) == 1 {
+		return &groups[0], nil
+	} else if len(groups) > 1 {
+		return nil, errors.New(consts.ErrorMultipleGroups)
+	}
+
+	return nil, errors.New(consts.ErrorNoGroupFound)
+}
+
+func getGroup(
+	ctx context.Context,
+	data GroupModel,
+	apiClient *sdk.APIClient,
+) (*sdk.ListGroups200ResponseAllOfGroupsInner, error) {
+	if !data.Id.IsNull() {
+		return getGroupByID(ctx, data.Id.ValueInt64(), apiClient)
+	} else if !data.Name.IsNull() {
+		return getGroupByName(ctx, data.Name.ValueString(), apiClient)
+	}
+
+	return nil, errors.New(consts.ErrorNoValidSearchTerms)
 }
 
 // Read refreshes the Terraform state with the latest data.
@@ -86,86 +127,24 @@ func (d *DataSource) Read(
 			summary,
 			"could not create sdk client",
 		)
+
+		return
 	}
 
-	var group sdk.ListGroups200ResponseAllOfGroupsInner
-
-	// Get by id
-	if !data.Id.IsNull() {
-		id := data.Id.ValueInt64()
-
-		g, hresp, err := apiClient.GroupsAPI.GetGroups(ctx, id).Execute()
-		if err != nil || hresp.StatusCode != http.StatusOK {
-			resp.Diagnostics.AddError(
-				summary,
-				fmt.Sprintf("group %d GET failed: ", id),
-			)
-
-			return
-		}
-
-		group = g.GetGroup()
-
-		goto found
-
-		// Get by name
-	} else if !data.Name.IsNull() {
-		name := data.Name.ValueString()
-
-		gs, hresp, err := apiClient.GroupsAPI.ListGroups(ctx).Name(name).Execute()
-		if err != nil || hresp.StatusCode != http.StatusOK {
-			resp.Diagnostics.AddError(
-				summary,
-				fmt.Sprintf("group %s GET failed: ", name),
-			)
-
-			return
-		}
-
-		var groups []sdk.ListGroups200ResponseAllOfGroupsInner
-
-		for _, g := range gs.Groups {
-			if g.GetName() == name {
-				groups = append(groups, g)
-			}
-		}
-
-		if len(groups) == 1 {
-			group = groups[0]
-
-			goto found
-
-		} else if len(groups) > 1 {
-			resp.Diagnostics.AddError(
-				summary,
-				consts.ErrorMultipleGroups,
-			)
-
-			return
-		}
-
-	} else {
+	group, err := getGroup(ctx, data, apiClient)
+	if err != nil {
 		resp.Diagnostics.AddError(
 			summary,
-			consts.ErrorNoValidSearchTerms,
+			err.Error(),
 		)
 
 		return
 	}
 
-	resp.Diagnostics.AddError(
-		summary,
-		consts.ErrorNoGroupFound,
-	)
-
-	return
-
-found:
-
-	data.Id = int64ToType(group.Id)
-	data.Name = strToType(group.Name)
-	data.Code = strToType(group.Code)
-	data.Location = strToType(group.Location)
+	data.Id = convert.Int64ToType(group.Id)
+	data.Name = convert.StrToType(group.Name)
+	data.Code = convert.StrToType(group.Code)
+	data.Location = convert.StrToType(group.Location)
 
 	diags = resp.State.Set(ctx, &data)
 	resp.Diagnostics.Append(diags...)
